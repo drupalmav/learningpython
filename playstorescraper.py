496f1d3e76c3b490ec98ead6da5398d289fe0e7f
drupalmav <mavdev32@gmail.com>
2015-12-13 16:03:01 -0600

    Updated playstoreapi.python, can search a keyword and get statistics

--- playstorescraper.py
+++ playstorescraper.py
@@ -4,51 +4,106 @@ Scraper for tracing how often an app is released.
 
 import urllib2
 from bs4 import BeautifulSoup
+import csv
 
-pkg = "com.wsandroid.suite"
+pkg = "com.mavdev.focusoutfacebook"
 url = "https://play.google.com/store/apps/details?id=" + pkg
-
 
 def get_app_lastupdate(pkg):
     url = "https://play.google.com/store/apps/details?id=" + pkg
-    html = urllib2.urlopen(url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    appTitle = soup.find("div", {"class": "document-title"}).text
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    data = opener.open(url).read()
+
+    soup = BeautifulSoup(data, 'html.parser')
+
+    appTitle = soup.find("h1", {"class": "document-title"}).text
     date = soup.find("div", {"itemprop": "datePublished"})
+    downloads = soup.find("div", {"itemprop": "numDownloads"})
+    rating = soup.find('div', {'class': 'score'}).text
+
+    reviews_list = list()
+
+    individualReviews = soup.find_all("div", {"class": "single-review"})
+    for oneReview in individualReviews:
+
+        reviewDict = dict()
+        author = oneReview.find('span', {'class': 'author-name'}).text
+        date = oneReview.find('span', {'class': 'review-date'}).text
+        link = "https://play.google.com/" + oneReview.find('a', {'class': 'reviews-permalink'}).get("href")
+        title = oneReview.find('span', {'class': 'review-title'}).text
+        body = oneReview.find('div', {'class': 'review-body'}).text
+        rating = oneReview.find('div', {'class': 'tiny-star star-rating-non-editable-container'}).text
+
+
+        reviewDict['author'] = author
+        reviewDict['date'] = date
+        reviewDict['link'] = link
+        reviewDict['title'] = title
+        reviewDict['body'] = body
+        reviewDict['rating'] = rating
+
+        reviews_list.append(reviewDict)
+
+    revCntStr = soup.find('span',{'class':'rating-count'}).text
+    revCntStr = revCntStr.replace(",", "")
+    reviewCnt = int(revCntStr)
+
+
 
     appDict = dict()
-    appDict["lastUpdaDate"] = date.text
-    appDict["name"] = appTitle
-    # appInfoList.append(date.text)
+    appDict['lastUpdaDate'] = date
+    appDict['name'] = appTitle
+    appDict['downloadCnt'] = downloads.text
+    appDict['reviewCnt'] = reviewCnt
+    appDict['reviews_list'] = reviews_list
+    appDict['rating'] = rating
+
+
+
+
     return appDict
 
-lastUpdate = get_app_lastupdate(pkg)
+def print_individual_appdetail(package):
+    lastUpdate = get_app_lastupdate(package)
 
-for item in lastUpdate:
-    print item
-    for k in lastUpdate[item]:
-        print k
-
-
-# titles = soup.find_all("h1", {"class", "document-title"})
-# print titles
+    print lastUpdate['name']
+    print lastUpdate['lastUpdaDate']
+    print lastUpdate['downloadCnt']
+    print lastUpdate['reviewCnt']
 
 
-# # job_titles = soup.find_all("div", {"class", "row"})
-# job_titles = soup.find_all("h2", {"class", "jobtitle"})
-# jobs_sponsored = soup.find_all("div", {"data-tn-component", "sponsoredJob"})
-#
-# for title in job_titles:
-#     print title.text.strip()
-#     #print title.contents[0].text
-#     # print title.contents[1].text
-#     # print title.contents[1].find_all("a", {"class", "jobtitle turnstileLink"})
-#     #print "\n"
-#
-#
-#
-# print "SPONSORED JOB LISTINGS"
-# print "\n"
-#
-# for sponsored in jobs_sponsored:
-#     print sponsored.text.strip()
+
+
+def searchappsbykeywords(keyword):
+
+    url = "https://play.google.com/store/search?q=focus&c=apps"
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    data = opener.open(url).read()
+
+    soup = BeautifulSoup(data, 'html.parser')
+
+    allapps = soup.find_all('div', {'class': 'card-content id-track-click id-track-impression'})
+
+    for eachapp in allapps:
+        #print eachapp
+        title = eachapp.find('a', {'class': 'title'}).text
+        for eachapp in allapps:
+            link = eachapp.find('a', {'class': 'title'}).get("href")
+            pkg = link.rsplit('=', 1)[1]
+            print_individual_appdetail(pkg)
+
+
+searchappsbykeywords("procrastination")
+
+'''
+#WRITING TO CSV FILE
+with open('eggs.csv', 'wb') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+'''
